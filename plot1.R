62f2f1f1bd706a767bd51338a98209798e8d57b5
cgravely <cindy.gravely@yahoo.com>
2014-10-11 16:39:59 -0600

    Updates

--- plot1.R
+++ plot1.R
@@ -17,6 +17,7 @@ plot1 <- function() {
   dat$one <- as.POSIXct(paste(dat$Date, dat$Time), format="%Y-%m-%d %H:%M:%S")
   
   # Create the plot
+  library(datasets)
   hist(dat$Global_active_power, main="Global Active Power",  xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
   dev.copy(png, file="plot1.png", height=480, width=480)
   dev.off()
