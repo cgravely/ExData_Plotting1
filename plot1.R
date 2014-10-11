d2cd2c141d6ac9e4e5f753984bfce9eff1f66d69
cgravely <cindy.gravely@yahoo.com>
2014-10-11 17:15:43 -0600

    Updates

--- plot1.R
+++ plot1.R
@@ -10,10 +10,11 @@ plot1 <- function() {
   fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   download.file(fileURL,temp)
   con <- unzip(temp, "household_power_consumption.txt")
-  dat <- read.csv.sql("household_power_consumption.txt", sql = "SELECT * from file WHERE Date IN ('1/2/2007','2/2/2007') ", sep = ";", header = TRUE)
+  dat <- read.csv.sql("household_power_consumption.txt", sql = "SELECT * from file WHERE Date IN ('2/1/2007','2/2/2007') ", sep = ";", header = TRUE)
   unlink(temp)
   
   # Create a new column that combines the date an time
+  dat$Date <- as.Date(dat$Date, "%m/%d/%Y")
   dat$one <- as.POSIXct(paste(dat$Date, dat$Time), format="%Y-%m-%d %H:%M:%S")
   
   # Create the plot
